a58176139e0680ae821b5bcf1afbf6ab979c2122
ashria2627 <ashria022@gmail.com>
2026-06-04 12:04:52 +0600

    corrected_dataset

--- dividing.py
+++ dividing.py
@@ -1,14 +1,23 @@
 import pandas as pd
 
-
-df = pd.read_csv('gramdoctor_triage_balanced.csv')
+df = pd.read_csv('gramdoctor_dataset_v2.csv')
 print("Total patients:", len(df))
 df['triage-no']=df['triage'].map({
     'green':0,
     'orange':1,
     'red':2
 })
-df.drop(['triage','jaw swelling'],inplace=True,axis=1)
+df['sex-no']=df['sex'].map({
+    'female':1,
+    'male':0
+})
+df['ispregnant']=df['pregnancy_status'].map({
+    'no':0,
+    'yes':1,
+    'not_applicable':2
+    
+})
+# df.drop(['sex','pregnancy_status','triage'],inplace=True,axis=1)
 # print(df)
 split_idx=int(0.8*len(df))
 train=df.iloc[:split_idx]
